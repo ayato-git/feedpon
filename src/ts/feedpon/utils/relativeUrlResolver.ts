2d69c3b0ac7055bc4d4137cd87d40678635ba9c7
Shota Nozaki <emonkak@gmail.com>
2014-11-11 23:20:14 +0900

    Fix the absolute url creation

--- src/ts/feedpon/utils/relativeUrlResolver.ts
+++ src/ts/feedpon/utils/relativeUrlResolver.ts
@@ -7,9 +7,17 @@ function isAbsoluteUrl(url: string): boolean {
     return /^(?:[a-z]+:)?\/\//.test(url);
 }
 
-function toAbsoluteUrl(url: string, documentUrl: ParsedUrl): string {
-    return documentUrl.protocol + '//' + documentUrl.host +
-        url.replace(/^\/*/, '/');
+function toAbsoluteUrl(relativeUrl: string, documentUrl: ParsedUrl): string {
+    if (relativeUrl[0] === '/') {
+        return documentUrl.protocol + '//' + documentUrl.host + relativeUrl;
+    } else {
+        // e.g. documentUrl: '/foo/bar/baz'
+        //      relativeUrl: 'qux'
+        //      => 'foo/bar/qux'
+        return documentUrl.protocol + '//' + documentUrl.host +
+            documentUrl.pathname.replace(/[^\/]*$/, '') +
+            relativeUrl.replace(/^\.\//, '');
+    }
 }
 
 export = relativeUrlResolver;
